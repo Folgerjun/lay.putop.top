67c5e776aa49e503034591cb74ed1adbae91723f
ffj <ffj0721@163.com>
2018-05-18 17:56:12 +0800

    修改导航栏js代码

--- src/main/resources/static/js/navigation.js
+++ src/main/resources/static/js/navigation.js
@@ -25,3 +25,31 @@ function showWeChatImg() {
 		  content: '/img/WeChat.png'
 	  });
 }
+
+/**
+ * 显示微信公众号二维码
+ * 
+ * @returns
+ */
+function showGZHImg() {
+	  layer.open({
+		  type: 2,
+		  title: '公众号二维码',
+		  area: ['260px', '310px'],
+		  content: '/img/GZH.jpg'
+	  });
+}
+
+/**
+ * 显示我的信息
+ * 
+ * @returns
+ */
+function showMe() {
+	  layer.open({
+		  type: 2,
+		  title: 'who i am ?',
+		  area: ['201px', '245px'],
+		  content: '/img/me.jpg'
+	  });
+}
